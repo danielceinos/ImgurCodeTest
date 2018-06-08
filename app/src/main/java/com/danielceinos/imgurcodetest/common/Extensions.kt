de553630050e3bd6e881ea5e27a6b113632c5cfb
Daniel Sanchez <daniel.ceinos@gmail.com>
2018-06-08 19:38:29 +0200

    Login layout done

---
+++ app/src/main/java/com/danielceinos/imgurcodetest/common/Extensions.kt
@@ -0,0 +1,10 @@
+package com.danielceinos.imgurcodetest.common
+
+import android.widget.EditText
+
+/**
+ * Created by Daniel S on 08/06/2018.
+ */
+
+
+fun EditText.text(): String = text.toString()
--- app/src/main/java/com/danielceinos/imgurcodetest/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/danielceinos/imgurcodetest/presentation/login/LoginActivity.kt
@@ -2,9 +2,12 @@ package com.danielceinos.imgurcodetest.presentation.login
 
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.danielceinos.imgurcodetest.R
+import com.danielceinos.imgurcodetest.common.text
+import com.danielceinos.imgurcodetest.databinding.ActivityLoginBinding
 import javax.inject.Inject
 
 class LoginActivity : AppCompatActivity() {
@@ -13,13 +16,15 @@ class LoginActivity : AppCompatActivity() {
   lateinit var mViewModelFactory: ViewModelProvider.Factory
 
   private lateinit var mViewModel: LoginViewModel
+  private lateinit var mBinding: ActivityLoginBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_login)
+    mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
     mViewModel = ViewModelProviders.of(this, mViewModelFactory)[LoginViewModel::class.java]
+    mViewModel.login("f25705520416faa", "2b1d60f0044e4c9f9bc061b65681612bcff5fd3e")
 
-    mViewModel.login("f25705520416faa", "2b1d60f0044e4c9f9bc061b65681612bcff5fd3e" )
+    mBinding.bLogin.setOnClickListener { mViewModel.login(mBinding.etEmail.text(), mBinding.etPassword.text()) }
   }
 }
